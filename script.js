93baf7a3ff0e5c5741c24e6e42bfaf805054eecf
jenniferjoyce94 <jennifer.joyce.jaglund@gmail.com>
2024-12-11 20:10:38 +0100

    Redy for presentation

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 const container = document.querySelector(".container");
 const quiz = document.querySelector(".quiz");
 const theQuestion = document.querySelector("#theQuestion");
-const alternativeBtn = document.querySelector("#alternativeBtn");
+const answerContainer = document.querySelector("#answerContainer");
 const nextBtn = document.querySelector(".nextBtn");
 const results = document.querySelector("#results");
+let darkMode = document.querySelector("#darkLightBtn");
 
 const myQuestions = [
-  // True/False Questions
   {
     question: "The Academy Awards are also known as the Oscars.",
     answers: ["True", "False"],
     correctAnswers: ["True"],
     type: "true-false",
   },
-
-  // Multiple Choice Questions
   {
     question: "Who has hosted the Oscars the most times?",
     answers: ["Billy Crystal", "Bob Hope", "Ellen DeGeneres", "Hugh Jackman"],
     correctAnswers: ["Bob Hope"],
     type: "multiple-choice",
   },
-
-  // Checkbox Questions
   {
     question:
       "Which of these movies have won Best Picture? (You can select multiple answers)",
     answers: ["Titanic", "The Godfather", "Citizen Kane", "Forrest Gump"],
     correctAnswers: ["Titanic", "The Godfather", "Forrest Gump"],
     type: "checkbox",
+  },
+  {
+    question: "Meryl Streep has won more Oscars than any other actor.",
+    answers: ["True", "False"],
+    correctAnswers: ["False"],
+    type: "true-false",
   },
 
   {
@@ -43,8 +45,89 @@ const myQuestions = [
     correctAnswers: ["Steven Spielberg", "Clint Eastwood"],
     type: "checkbox",
   },
+  {
+    question:
+      "Which of these actors have won an Oscar for Best Actor? (You can select multiple answers)",
+    answers: [
+      "Leonardo DiCaprio",
+      "Brad Pitt",
+      "Tom Hanks",
+      "Denzel Washington",
+    ],
+    correctAnswers: ["Leonardo DiCaprio", "Tom Hanks", "Denzel Washington"],
+    type: "checkbox",
+  },
+  {
+    question: "The first Oscars ceremony was held in 1929.",
+    answers: ["True", "False"],
+    correctAnswers: ["True"],
+    type: "true-false",
+  },
+  {
+    question: "Who won the Oscar for Best Actor at the 	2023 Oscars?",
+    answers: [
+      "Austin Butler (Elvis)",
+      "Colin Farrell (The Banshees of Inisherin)",
+      "Brendan Fraser (The Whale)",
+      "Paul Mescal (Aftersun)",
+    ],
+    correctAnswers: ["Brendan Fraser (The Whale)"],
+    type: "multiple-choice",
+  },
+  {
+    question:
+      "Which of these films were directed by Steven Spielberg? (You can select multiple answers)",
+    answers: [
+      "Jaws",
+      "Schindler's List",
+      "E.T. the Extra-Terrestrial",
+      "Pulp Fiction",
+    ],
+    correctAnswers: ["Jaws", "Schindler's List", "E.T. the Extra-Terrestrial"],
+    type: "checkbox",
+  },
+  {
+    question:
+      "Which animated movie was the first to be nominated for Best Picture?",
+    answers: ["Toy Story", "Beauty and the Beast", "The Lion King", "Shrek"],
+    correctAnswers: ["Beauty and the Beast"],
+    type: "multiple-choice",
+  },
+  {
+    question: "Which film is the most awarded in Oscars history?",
+    answers: [
+      "La La Land",
+      "Titanic",
+      "The Lord of the Rings: The Return of the King",
+      "Ben-Hur",
+    ],
+    correctAnswers: ["The Lord of the Rings: The Return of the King"],
+    type: "multiple-choice",
+  },
+  {
+    question:
+      "Which movies won the Big Five Oscars (Picture, Director, Actor, Actress, Screenplay)? (You can select multiple answers)",
+    answers: [
+      "The Silence of the Lambs",
+      "Titanic",
+      "One Flew Over the Cuckoo's Nest",
+      "It Happened One Night",
+    ],
+    correctAnswers: [
+      "The Silence of the Lambs",
+      "One Flew Over the Cuckoo's Nest",
+      "It Happened One Night",
+    ],
+    type: "checkbox",
+  },
 ];
 
+darkMode.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  darkLightBtn.innerHTML = document.body.classList.contains("dark-mode")
+    ? "☀️"
+    : "🌙";
+});
 let questionIndex;
 let score;
 let selectedAnswer;
@@ -58,24 +141,21 @@ function startQuiz() {
   displayQuestion();
 }
 
-let darkMode = document.querySelector("#darkLightBtn");
-darkMode.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  darkLightBtn.innerHTML = document.body.classList.contains("dark-mode")
-    ? "🌙"
-    : "☀️";
-});
-
 function displayQuestion() {
   resetState();
   const currentQuestion = myQuestions[questionIndex];
   theQuestion.textContent = `${questionIndex + 1}. ${currentQuestion.question}`;
-
   if (currentQuestion.type === "checkbox") {
     createCheckboxOptions(currentQuestion.answers);
   } else {
     createAnswerButtons(currentQuestion.answers);
   }
+}
+
+function resetState() {
+  nextBtn.style.display = "none";
+  answerContainer.innerHTML = "";
+  selectedAnswer = null;
 }
 
 function createAnswerButtons(answers) {
@@ -84,7 +164,7 @@ function createAnswerButtons(answers) {
     button.textContent = answer;
     button.classList.add("answerBtn");
     button.addEventListener("click", () => selectAnswer(button));
-    alternativeBtn.appendChild(button);
+    answerContainer.appendChild(button);
   });
 }
 
@@ -105,37 +185,29 @@ function createCheckboxOptions(answers) {
 
     checkboxContainer.appendChild(checkbox);
     checkboxContainer.appendChild(label);
-    alternativeBtn.appendChild(checkboxContainer);
+    answerContainer.appendChild(checkboxContainer);
   });
 }
 
-function resetState() {
-  nextBtn.style.display = "none";
-  alternativeBtn.innerHTML = "";
-  selectedAnswer = null;
-}
-
 function selectAnswer(button) {
-  if (
-    myQuestions[questionIndex].type === "multiple-choice" ||
-    myQuestions[questionIndex].type === "true-false"
-  ) {
-    Array.from(alternativeBtn.children).forEach((btn) => {
-      btn.classList.remove("selected");
-    });
-    button.classList.add("selected");
-    selectedAnswer = button.innerHTML;
-    nextBtn.style.display = "block";
-  } else if (myQuestions[questionIndex].type === "checkbox") {
+  if (myQuestions[questionIndex].type === "checkbox") {
     selectedAnswer = Array.from(
-      alternativeBtn.querySelectorAll("input[type='checkbox']:checked")
+      answerContainer.querySelectorAll("input[type='checkbox']:checked")
     ).map((checkbox) => checkbox.value);
+
     if (selectedAnswer.length !== 0) {
       nextBtn.style.display = "block";
     } else {
       nextBtn.style.display = "none";
     }
+    return;
   }
+  Array.from(answerContainer.children).forEach((btn) => {
+    btn.classList.remove("selected");
+  });
+  button.classList.add("selected");
+  selectedAnswer = button.innerHTML;
+  nextBtn.style.display = "block";
 }
 
 nextBtn.addEventListener("click", () => {
@@ -175,15 +247,15 @@ function showResults() {
 
   if (getProcent < 50) {
     resultMessage = "Oops! Seems like you had a blackout during the movie.";
-    resultColor = "red";
+    resultColor = "darkred";
   } else if (getProcent >= 50 && getProcent <= 75) {
     resultMessage =
-      "You know most things, but maybe you missed the end credits!";
-    resultColor = "orange";
+      "You know most movies, but maybe you fell asleep before the end!";
+    resultColor = "darkorange";
   } else {
     resultMessage =
       "PERFECTION! You are like Quentin Tarantino and Steven Spielberg all in one person!";
-    resultColor = "green";
+    resultColor = "darkgreen";
   }
 
   results.innerHTML = `
@@ -199,13 +271,17 @@ function showResults() {
   playAgain.onclick = startQuiz;
 
   const seeAnswers = document.createElement("button");
-  seeAnswers.innerHTML = "See you anwsers here";
+  seeAnswers.innerHTML = "See your anwsers here";
   seeAnswers.classList.add("nextBtn");
   seeAnswers.style.display = "block";
+
   seeAnswers.addEventListener("click", () => {
-    console.log("Click");
-    const displayResult = showEndResults();
-    results.appendChild(displayResult);
+    if (!document.querySelector(".answers-shown")) {
+      const displayResult = showEndResults();
+      displayResult.classList.add("answers-shown");
+      results.appendChild(displayResult);
+    }
+    seeAnswers.disabled = "true";
   });
 
   results.appendChild(seeAnswers);
